31bda3eef4a3d0f08a6e31cbd2c849f613071a15
Daniel-s-Walker <pajamadan87@gmail.com>
2016-12-13 08:56:23 -0500

    Badda boom! Badda Bing!
    :dance:

--- Weapon.java
+++ Weapon.java
@@ -7,8 +7,8 @@ import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 
 public class Weapon extends Item{
-   public BufferedImage img = null;
-   private String wType;
+  public BufferedImage img = null;
+  private String wType;
   private String[][] weaponCatalogue ={
     {"Pistol","Chicken Cooper","Six-Shooter","Super Shooter","Brigadier","The Good Boy Gun"},
     {"Rifle","L.A.R.D.","Screwball","Big Man","Bolt Action Riflee"},
@@ -20,6 +20,14 @@ public class Weapon extends Item{
     {"BBGun","B.B. Boy"}
   }; 
   
+  
+  //1 defaults, yo.
+  private int dmglo = 1;  
+  private int dmghi = 1;
+  private int clip = 1;
+  private int range = 1;
+  private int price = 1;
+  
   public Weapon(){
     int i = (int) (8*Math.random());
     int j = (int) ((weaponCatalogue[i].length-1)*Math.random()+1);
@@ -28,8 +36,30 @@ public class Weapon extends Item{
     description = "Yeehaw! What a VERY WELL MADE weaponry device!";
     price = 6;
     wType = weaponCatalogue[i][0];
+     try {
+            setData();
+        } catch (IOException e) {
+            System.out.println("setData() failed, default stats kept.");
+        }
+    try{
+      img = ImageIO.read(new File("images/items/weapons/" + wType + ".png")); 
+    }
+    catch(IOException e){     
+      System.out.println("Hey hey!There's no image for " + wType);
+    }
     
-        try{
+  }
+  
+  public Weapon(String n, String c){
+    name = n;
+     wType = c;
+     
+     try {
+            setData();
+        } catch (IOException e) {
+            System.out.println("setData() failed, default stats kept.");
+        }
+    try{
       img = ImageIO.read(new File("images/items/weapons/" + wType + ".png")); 
     }
     catch(IOException e){     
@@ -37,8 +67,30 @@ public class Weapon extends Item{
     }
   }
   
+  public void setData() throws IOException{
+    BufferedReader reader = null;
+    try {
+      File file = new File("gamedata/itemStats/" + wType + "/" +  name + ".txt");
+      reader = new BufferedReader(new FileReader(file));
+    }
+    catch (FileNotFoundException e) {
+      System.out.println("gamedata/itemStats/" + wType + "/" +  name + ".txt");
+      throw new IOException();
+    }
+    
+    dmglo = Integer.parseInt(reader.readLine());
+    dmghi = Integer.parseInt(reader.readLine());
+    clip = Integer.parseInt(reader.readLine());
+    range = Integer.parseInt(reader.readLine());
+    price = Integer.parseInt(reader.readLine());
+  }
+  
+  public int rollDamage(){
+   return (int) (Math.random() * dmghi) + dmglo; 
+  }
+  
   public void use(CharacterSheet c){
-    c.str += 100;
+    c.weap = this;
   }
   
   public void paint(Graphics2D g2d, int x, int y){
