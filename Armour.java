ba14a2056e2143d91096fbcfc8d1d126c3620187
Daniel-s-Walker <pajamadan87@gmail.com>
2016-12-14 13:15:42 -0500

    gotta go!!!
    :sunglasses:

--- Armour.java
+++ Armour.java
@@ -11,17 +11,19 @@ public class Armour extends Item{
   public BufferedImage img = null;
   private String equipType;
   private String[][] armourCatalogue ={
-    {"Armour","Plain Clothes","Leather Armour","Bandit Outfit","High Grade Leather","Deputy's Outfit","Plain Robes","Cultist Robes","Magic Armour","Plate Mail","Dress Clothes","Governer's Dress","Red Dragon Hide","Brass Dragon Hide"},
+    {"Clothing","Plain Clothes","Bandit Outfit","Deputy's Outfit","Dress Clothes","Governer's Dress"},
+    {"Robes","Plain Robes","Cultist Robes"},
+    {"Armour","Leather Armour","High Grade Leather","Magic Armour","Plate Mail","Red Dragon Hide","Brass Dragon Hide"},
     {"Hat","Cowboy Hat","Wizard Hat","Metal Helmet","Fancy Hat","Senior Wizard Hat","Deputy's Ten Gallon","Army Surplus Helmet","Twenty Gallon Hat","Very Good Wizard Hat","Arachnid Gun Hat","Solar Hat","Red Dragon's Head","Brass Dragon's Head"}
   };
   
    
    public Armour() {
-       int i = (int) (2*Math.random());
-       int j = (int) (13*Math.random()+1);
+       int i = (int) (4*Math.random());
+       int j = (int) ((armourCatalogue[i].length-1)*Math.random()+1);
        equipType = armourCatalogue[i][0];
        type = "Armour";
-       if(equipType == "Armour"){
+       if(equipType == "Armour" || equipType == "Clothing" || equipType == "Robes"){
        
         description = "A Nice Set of Armour";
        }
